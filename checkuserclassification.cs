50bd74171ec299845be86cef92b95a3b75ea70a7
SSDRJ <94144551+SSDRJ@users.noreply.github.com>
2023-11-06 15:30:41 -0600

    comments for new file

---
+++ checkuserclassification.cs
@@ -0,0 +1,19 @@
+//Header is sender
+//Footer is receiver
+//read and validate
+//Header is what needs regex, split them up and check each
+//footer is the permissions of receiver, use regex to split this.
+//grab users current permissions, split them into regex 
+// There are multiple sections, the split in sections are indicatted with either '//' or '/'
+
+//1. check the values of the person who is sending and receiving the email
+//2. compare the two values together
+//3. If the sender does not match the receiver then the email is not allowed to be sent
+
+using System;
+
+
+namespace checkuserclassification{
+
+
+}
