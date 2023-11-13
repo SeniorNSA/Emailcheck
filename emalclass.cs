c65d5677c0d6decc21ea0f093dc35782c2251c13
pirana3 <piran356@gmail.com>
2023-11-13 00:13:11 -0600

    change what is needed for emails

--- emalclass.cs
+++ emalclass.cs
@@ -5,48 +5,54 @@ namespace SecurityClassifications
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            Dictionary<string, string> emailClass = new Dictionary<string, string>(); // this is what the if statment is about
-            Dictionary<string, string> employeeRoles = new Dictionary<string, string> // this is what is defined 
+            Dictionary<string, string> emailClass = new Dictionary<string, string>();
+            Dictionary<string, string> employeeRoles = new Dictionary<string, string>
             {
                 {"NSAProgramTester1@outlook.com", "Head of Department"},
                 {"NSAProgramTester2@outlook.com", "Employee"},
                 {"NSAProgramTester3@outlook.com", "Intern"}
             };
 
-            foreach (var entry in employeeRoles)// gets the values
+            // Populate emailClass dictionary based on roles
+            foreach (var entry in employeeRoles)
             {
-                if (entry.Value == "head of the department")// checks the values and classifies them
+                if (entry.Value.Equals("Head of Department", StringComparison.OrdinalIgnoreCase))
                 {
-                    emailClass[entry.Key] = "TOP SECRET, SECRET, CONFIDENTIAL, UNCLASSIFIED";
-                    emailClass[entry.Key] = "HCS, COMINT, -GAMMA, -ECI, TALENT KEYHOLE";
-                    emailClass[entry.Key] = "TS, S, C , U";
-                    emailClass[entry.Key] = "HCS, SI, -G, -ECI XXX, TK";
+                    emailClass[entry.Key] = "TOP SECRET, SECRET, CONFIDENTIAL, UNCLASSIFIED, HCS, COMINT, -GAMMA, -ECI, TALENT KEYHOLE, TS, S, C , U, HCS, SI, -G, -ECI XXX, TK";
                 }
-                else if (entry.Value == "Employee")
+                else if (entry.Value.Equals("Employee", StringComparison.OrdinalIgnoreCase))
                 {
-                    emailClass[entry.Key] = "SECRET, CONFIDENTIAL, UNCLASSIFIED";
-                    emailClass[entry.Key] = "HCS, COMINT, TALENT KEYHOLE";
-                    emailClass[entry.Key] = "S, C, U";
-                    emailClass[entry.Key] = "HCS, SI, TK";
+                    emailClass[entry.Key] = "SECRET, CONFIDENTIAL, UNCLASSIFIED, HCS, COMINT, TALENT KEYHOLE, C, U, HCS, SI, TK";
                 }
-                else if (entry.Value == "Intern")
+                else if (entry.Value.Equals("Intern", StringComparison.OrdinalIgnoreCase))
                 {
-                    emailClass[entry.Key] = " CONFIDENTIAL, UNCLASSIFIED";
-                    emailClass[entry.Key] = "HCS, COMINT";
-                    emailClass[entry.Key] = " C, U";
-                    emailClass[entry.Key] = "HCS, SI";
+                    emailClass[entry.Key] = "CONFIDENTIAL, UNCLASSIFIED, HCS, COMINT, C, U, HCS, SI";
                 }
-                
             }
 
-            foreach (var entry in emailClass)//prints with the classification.
+            string line;
+            while ((line = Console.ReadLine()) != null)
             {
-                Console.WriteLine($"Email: {entry.Key} | Classification: {entry.Value}");
+                // Check for the delimiter line
+                if (line.Contains("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------"))
+                {
+                    break;
+                }
+
+                // Extract sender and receiver from the line
+                var parts = line.Split(new string[] { "Sender:", "Receiver:" }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length < 2) continue; // Skip if the format is incorrect
+
+                string senderEmail = parts[0].Trim();
+                string receiverEmail = parts[1].Trim();
+
+                // Determine the classification based on the sender's email
+                string classification = emailClass.TryGetValue(senderEmail, out var value) ? value : "Unknown";
+
+                Console.WriteLine($"Sender: {senderEmail} | Receiver: {receiverEmail} | Classification: {classification}");
             }
         }
     }
 }
-
-
